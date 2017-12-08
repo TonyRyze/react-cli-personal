2ece19de046d8edcdd5784f735b4835f0dd3a0b9
lishaojie <108274244@qq.com>
2017-12-08 18:35:07 +0800

    react-cli

---
+++ src/redux/store/configureStore.dev.js
@@ -0,0 +1,25 @@
+import { createStore, compose } from 'redux'
+import rootReducer from '../reducers/index'
+import middlewares from './middlewares'
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const enhancer = composeEnhancers(middlewares)
+
+const devToolsExtension = window.devToolsExtension.open();
+
+export default function configureStore (initialState) {
+  const store = createStore(
+    rootReducer,
+    initialState,
+    enhancer
+  )
+
+  if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      const nextRootReducer = require('../reducers/index').default
+      store.replaceReducer(nextRootReducer)
+    })
+  }
+
+  return store
+}
